90d3526ae3b9b6d07d998ae0b24d022451dfcb85
Daniel <danne_1998@hotmail.se>
2021-04-20 14:31:15 +0200

    linter

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import { FirebaseConfig } from './firebaseConfig';
+import {FirebaseConfig} from './firebaseConfig';
 
 const config = {
     apiKey: FirebaseConfig.apiKey,
