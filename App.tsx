ef17596fd626653bfbdec2be8c37031933aa2457
YosefAmin <yosefamin3567@gmail.com>
2021-04-20 11:00:00 +0200

    test

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <StatusBar style="dark"/>
+        <Text>PaddlePal</Text>
+      </View>
+    </NavigationContainer>
   );
 }
 
