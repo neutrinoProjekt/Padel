95eb967129bf57a363b6397db4f0324887736fde
Daniel <danne_1998@hotmail.se>
2021-04-21 11:48:03 +0200

    add auth context with functioning login, logut and signup

---
+++ src/modules/firebase/firebaseConfig.tsx
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+    apiKey: 'AIzaSyAY04oKZKcoi3MoQvKjHlmLRLdND9Cw0uY',
+    authDomain: 'padel-polare.firebaseapp.com',
+    projectId: 'padel-polare',
+    storageBucket: 'padel-polare.appspot.com',
+    messagingSenderId: '501518002657',
+    appId: '1:501518002657:web:91c03668f188be4abb4aa0',
+};
