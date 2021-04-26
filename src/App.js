24f0d95f6532b687731ad9d02677db7c0b673456
Philip Salqvist <>
2021-04-26 16:48:17 +0200

    Add BottomNavigation to App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import StartScreen from './screens/home/StartScreen';
 import LoginScreen from './screens/login/LoginScreen';
+import BottomNavigation from './screens/bottomNav/BottomNavigation';
 import {RegistrationContainer} from './screens/registration/RegistrationContainer';
 import {AuthProvider} from './contexts/auth';
 
@@ -19,6 +20,7 @@ export default function App() {
                     <Stack.Screen name="Home" component={StartScreen} />
                     <Stack.Screen name="Registration" component={RegistrationContainer} />
                     <Stack.Screen name="Login" component={LoginScreen}/>
+                    <Stack.Screen name="PaddlePal" component={BottomNavigation}/>
                 </Stack.Navigator>
             </NavigationContainer>
         </AuthProvider>
