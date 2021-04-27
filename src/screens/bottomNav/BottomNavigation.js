10c7b8514b72d82daeb21103237cbe2bd936d9b1
Daniel <danne_1998@hotmail.se>
2021-04-27 16:01:50 +0200

    fix merge

--- src/screens/bottomNav/BottomNavigation.js
+++ src/screens/bottomNav/BottomNavigation.js
@@ -4,7 +4,7 @@ import * as React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Ionicons} from '@expo/vector-icons';
 import HomeScreen from '../home/HomeScreen';
-import PersonPageScreen from '../PersonPageScreen';
+import PersonPageScreen from '../personPage/PersonPageScreen';
 import Notifications from '../notifiactions/Notifications'
 
 const Tab = createBottomTabNavigator();
@@ -44,11 +44,7 @@ const BottomNavigation = () => {
 
             initialRouteName="HomeScreen"
         >
-
-<<<<<<< HEAD
-=======
             <Tab.Screen name="Notifications" component={Notifications} />
->>>>>>> origin/main
             <Tab.Screen name="Profile" component={PersonPageScreen} />
             {/* Replace Tournament and Notifications with your screens */}
 
@@ -56,15 +52,7 @@ const BottomNavigation = () => {
             {/* <Tab.Screen name="Tournament" component={Tournament} />
             <Tab.Screen name="Notifications" component={Notifications} />
             <Tab.Screen name="Profile" component={Profile} /> */}
-
-<<<<<<< HEAD
-=======
-            
-
-
->>>>>>> origin/main
         </Tab.Navigator>
-
     );
 };
 
