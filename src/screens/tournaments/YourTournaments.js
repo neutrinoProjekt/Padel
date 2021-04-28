5f04c4566063f3a34c333855f5906d8e4a458e66
ShaffZ <sean.zakko@yahoo.com>
2021-04-28 16:38:41 +0200

    new tour file

--- src/screens/tournaments/YourTournaments.js
+++ src/screens/tournaments/YourTournaments.js
@@ -1,89 +1,117 @@
-import React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet, TouchableOpacity, View}
+import React, {useState} from 'react';
+import {FlatList, Text, SafeAreaView, ScrollView, StyleSheet, TouchableHighlight, View}
     from 'react-native';
-import TournamentItem from '../../components/TournamentItem';
-import {Ionicons} from '@expo/vector-icons';
 import {createMaterialTopTabNavigator}
     from '@react-navigation/material-top-tabs';
+//import { FlatList } from 'react-native-gesture-handler';
+import {ListItem} from 'react-native-elements';
+
 
 //temporary data until fetching from firebase
-const getTournaments = () => (
-    [
-        {
-            id: 'ma1',
-            owner: {
-                id: 'us1',
-                name: 'Karl-Bertil Johansson',
-                imageUri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
-            },
-            participants: [],
+const DATA = [
+    {
+        id: 'ma1',
+        owner: {
+            id: 'us1',
+            name: 'Karl-Bertil Johansson',
+            imageUri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
         },
-        {
-            id: 'ma2',
-            owner: {
-                id: 'us1',
-                name: 'Anna-Karin Johansson',
-                imageUri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
-            },
-            participants: [],
+        participants: [],
+    },
+    {
+        id: 'ma2',
+        owner: {
+            id: 'us1',
+            name: 'Anna-Karin Johansson',
+            imageUri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
         },
-        {
-            id: 'ma3',
-            owner: {
-                id: 'us1',
-                name: 'Britt-Marie Johansson',
-                imageUri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
-            },
-            participants: [],
+        participants: [],
+    },
+    {
+        id: 'ma3',
+        owner: {
+            id: 'us1',
+            name: 'Britt-Marie Johansson',
+            imageUri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
         },
-    ]
-);
+        participants: [],
+    },
+];
 
-const TopNavigator = createMaterialTopTabNavigator();
+//const TopNavigator = createMaterialTopTabNavigator();
 
-const YourTournaments = ({navigation}) => {
+
+const TournamentItem = ({item , onPress}) => {
+
+    const [extend, setExtend] = useState('');
+
     return (
-        <SafeAreaView>
-            <ScrollView style={styles.container}>
-                {
-                    getTournaments().map((tour) => (
-                        <TournamentItem
-                            key={tour.id}
-                            owner={tour.owner}
-                            participants={tour.participants}
-                        />
-                    ))
-                }
-            </ScrollView>
-        </SafeAreaView>
-
+        <TouchableHighlight onPress={() => {setExtend(!extend)}} >      
+        <View>
+            <ListItem topDivider>
+                <ListItem.Content style={styles.listItemOne}>
+                    <ListItem.Title style={{fontWeight: "800"}}>
+                        Match Created By: {item.owner.name}
+                        Participants are: {item.participants}
+                    </ListItem.Title>
+                </ListItem.Content>
+            </ListItem>
+            <ListItem topDivider style={styles.listItemTwo}>
+                <ListItem.Content>
+                    <ListItem.Title style={{fontWeight: "800"}}>
+                        Match Created By: {item.owner.name}
+                    </ListItem.Title>
+                </ListItem.Content>
+            </ListItem>
+        </View>
+        </TouchableHighlight>
     );
 };
 
-export default YourTournaments;
+
+const TournamentsList = () => {
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList                
+            data={DATA}
+            renderItem={({ item }) => (
+                <TournamentItem 
+                  item={item}
+                />
+              )}
+            keyExtractor={item => item.id}           
+            />
+            <Text style={styles.nEnd}>No more tournaments to show</Text>
+        </SafeAreaView>
+    );
+};
+
+export default TournamentsList;
+
+
+
+
 
 const styles = StyleSheet.create({
     container: {
         height: '100%',
         backgroundColor: 'white',
     },
-    actionButtonContainer: {
+
+    listItemOne: {
         flex: 1,
-        position: 'absolute',
-        bottom: 20,
-        right: 20,
-    },
-    actionButton: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: 50,
         height: 50,
-        backgroundColor: 'white',
-        borderRadius: 100,
-        shadowRadius: 10,
-        shadowColor: 'black',
-        shadowOpacity: 0.3,
-        shadowOffset: {height: 10},
+        backgroundColor: '#F7F7F7',
+    },
+    listItemTwo: {
+        flex: 1,
+        height: 100,
+    },
+
+    nEnd: {
+        color: '#707070',
+        textAlign: 'center',
+        margin: 50,
     },
 
 });
