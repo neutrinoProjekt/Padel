ee71669719fdac017cb20d72fff4d7f837fce147
ShaffZ <sean.zakko@yahoo.com>
2021-04-28 17:50:30 +0200

    more tour

--- src/screens/tournaments/YourTournaments.js
+++ src/screens/tournaments/YourTournaments.js
@@ -1,10 +1,9 @@
 import React, {useState} from 'react';
-import {FlatList, Text, SafeAreaView, ScrollView, StyleSheet, TouchableHighlight, View}
+import {FlatList, Text, SafeAreaView, ScrollView, StyleSheet,TouchableOpacity, TouchableHighlight, View}
     from 'react-native';
-import {createMaterialTopTabNavigator}
-    from '@react-navigation/material-top-tabs';
-//import { FlatList } from 'react-native-gesture-handler';
-import {ListItem} from 'react-native-elements';
+import {ListItem, Divider, Avatar} from 'react-native-elements';
+import {Ionicons} from '@expo/vector-icons';
+
 
 
 //temporary data until fetching from firebase
@@ -36,6 +35,53 @@ const DATA = [
         },
         participants: [],
     },
+    {
+        id: 'ma4',
+        owner: {
+            id: 'us1',
+            name: 'Britt-Marie Johansson',
+            imageUri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
+        },
+        participants: [],
+    },    
+    {
+        id: 'ma5',
+        owner: {
+            id: 'us1',
+            name: 'Britt-Marie Johansson',
+            imageUri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
+        },
+        participants: [],
+    },    
+    {
+        id: 'ma6',
+        owner: {
+            id: 'us1',
+            name: 'Britt-Marie Johansson',
+            imageUri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
+        },
+        participants: [],
+    },    
+    {
+        id: 'ma7',
+        owner: {
+            id: 'us1',
+            name: 'Britt-Marie Johansson',
+            imageUri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
+        },
+        participants: [],
+    },
+    {
+        id: 'ma8',
+        owner: {
+            id: 'us1',
+            name: 'Bre Johansson',
+            imageUri: 'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
+        },
+        participants: [],
+    },
+    
+    
 ];
 
 //const TopNavigator = createMaterialTopTabNavigator();
@@ -47,22 +93,95 @@ const TournamentItem = ({item , onPress}) => {
 
     return (
         <TouchableHighlight onPress={() => {setExtend(!extend)}} >      
-        <View>
-            <ListItem topDivider>
-                <ListItem.Content style={styles.listItemOne}>
-                    <ListItem.Title style={{fontWeight: "800"}}>
-                        Match Created By: {item.owner.name}
-                        Participants are: {item.participants}
-                    </ListItem.Title>
-                </ListItem.Content>
-            </ListItem>
-            <ListItem topDivider style={styles.listItemTwo}>
+            <View>
+            <ListItem containerStyle={styles.listItemOne}>
+                <Avatar
+                    size={50}
+                    rounded
+                    source={{
+                        uri:
+                        'https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png',
+                    }}
+                />
                 <ListItem.Content>
-                    <ListItem.Title style={{fontWeight: "800"}}>
-                        Match Created By: {item.owner.name}
+                    <ListItem.Title
+                        style={styles.title}
+                        numberOfLines={1}
+                        ellipsizeMode='tail'
+                    >
+                        Created By: {item.owner.name}
                     </ListItem.Title>
+                    <ListItem.Subtitle style={styles.subTitle1}>
+                        0735623578
+                    </ListItem.Subtitle>
+                    <ListItem.Subtitle style={styles.subTitle2}>
+                        Ranking: 1438
+                    </ListItem.Subtitle>
+                </ListItem.Content>
+                <TouchableOpacity>
+                    <Ionicons
+                        size={10}
+                        name='ellipsis-horizontal'
+                        color='#707070'
+                        padding={2}
+                    />
+                </TouchableOpacity>
+            </ListItem>
+            <Divider/>
+            <ListItem containerStyle={styles.listItemTwo}>
+                <ListItem.Content>
+                    <View style={styles.rowContainer}>
+                        <Ionicons
+                            size={15}
+                            name='time-outline'
+                            color='#707070'
+                        />
+                        <ListItem.Subtitle style={styles.subTitle1}>
+                            2021-06-11, 17:00-20:00
+                        </ListItem.Subtitle>
+                    </View>
+                    <View style={styles.rowContainer}>
+                        <View marginLeft={-1}>
+                            <Ionicons
+                                size={14}
+                                name='location-outline'
+                                color='#707070'
+                            />
+                        </View>
+                        <ListItem.Subtitle style={styles.subTitle1}>
+                            Södertälje Padelhall, Stockholm
+                        </ListItem.Subtitle>
+                    </View>
+                    <ListItem.Subtitle style={styles.subTitle3}>
+                        Players attending so far:
+                    </ListItem.Subtitle>
+                    <View style={styles.rowContainer}>
+                        <View style={styles.columnContainer}>
+                            <ListItem.Subtitle style={styles.name}>
+                                Johan Petersson
+                            </ListItem.Subtitle>
+                            <ListItem.Subtitle style={styles.name}>
+                                Johan Persson
+                            </ListItem.Subtitle>
+                            <ListItem.Subtitle style={styles.name}>
+                                Johan Persson
+                            </ListItem.Subtitle>
+                        </View>
+                        <View style={styles.columnContainer}>
+                            <ListItem.Subtitle style={styles.ranking}>
+                                Ranking: 1048
+                            </ListItem.Subtitle>
+                            <ListItem.Subtitle style={styles.ranking}>
+                                Ranking: 2034
+                            </ListItem.Subtitle>
+                            <ListItem.Subtitle style={styles.ranking}>
+                                Ranking: 1567
+                            </ListItem.Subtitle>
+                        </View>
+                    </View>
                 </ListItem.Content>
             </ListItem>
+            <Divider/>
         </View>
         </TouchableHighlight>
     );
@@ -90,28 +209,76 @@ export default TournamentsList;
 
 
 
-
-
 const styles = StyleSheet.create({
     container: {
         height: '100%',
         backgroundColor: 'white',
     },
-
-    listItemOne: {
-        flex: 1,
-        height: 50,
-        backgroundColor: '#F7F7F7',
-    },
-    listItemTwo: {
-        flex: 1,
-        height: 100,
-    },
-
     nEnd: {
         color: '#707070',
         textAlign: 'center',
         margin: 50,
+    },
+    columnContainer: {
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 4,
+        marginBottom: 4,
+    },
+    listItemOne: {
+        flex: 1,
+        height: 70,
+        backgroundColor: '#F7F7F7',
+    },
+    listItemTwo: {
+        flex: 1,
+        height: 175,
+    },
+    title: {
+        paddingTop: 10,
+        paddingLeft: 2,
+        color: '#707070',
+        fontWeight: '800',
+        fontSize: 12,
+    },
+    subTitle1: {
+        paddingLeft: 2,
+        color: '#707070',
+        fontWeight: '700',
+        fontSize: 10,
+    },
+    subTitle2: {
+        paddingBottom: 5,
+        padding: 2,
+        color: '#00CEB4',
+        fontWeight: '700',
+        fontSize: 10,
+    },
+    subTitle3: {
+        paddingTop: 5,
+        paddingBottom: 5,
+        color: '#707070',
+        fontWeight: '600',
+        fontSize: 12,
+    },
+    name: {
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingRight: 20,
+        color: '#707070',
+        fontWeight: '700',
+        fontSize: 10,
+    },
+    ranking: {
+        paddingTop: 5,
+        paddingBottom: 5,
+        color: '#00CEB4',
+        fontWeight: '700',
+        fontSize: 10,
     },
 
 });
