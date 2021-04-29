519233295e54b9668b47d7c61fbaaa7273ccb3f0
YosefAmin <yosefamin3567@gmail.com>
2021-04-29 13:06:13 +0200

    revert package-lock

--- src/screens/tournament/CreateTournamentScreen.js
+++ src/screens/tournament/CreateTournamentScreen.js
@@ -90,7 +90,6 @@ const CreateTournamentScreen = () => {
 
     // Returns date
     const getDate = (date) => {
-        console.log(date);
         date = date.toString().substring(0, 15).split(' ');
         return date[3] + '-' + date[1] + '-' + date[2];
     };
@@ -126,7 +125,6 @@ const CreateTournamentScreen = () => {
         // Validate rank interval
         if (toggle1 && toggle2 && !validRankInterval()) {
             setErrorMsg('Invalid rank interval');
-            return;
         };
 
         // Validate date
