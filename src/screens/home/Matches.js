d95b0ae81d907dfdf4531283c6eae322087ac0db
Philip Salqvist <>
2021-05-04 08:48:03 +0200

    Add new Matches structure

--- src/screens/home/Matches.js
+++ src/screens/home/Matches.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import MainFormInput from './../../components/MainFormInput';
+import {StyleSheet, Text, SafeAreaView} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import BackButton from '../../components/BackButton';
 
-const Matches = () => {
+const Matches = ({navigation}) => {
     return (
-        <View>
-            <MainFormInput value={'Söderköping'} placeholder={'Specify city'} inputTitle={'City'} inputWidth={305}></MainFormInput>
-            <Text>This is the Matches Screen!</Text>
-        </View>
+        <SafeAreaView style={styles.safeContainer}>
+            <ScrollView style={styles.scrollContainer}>
+                <BackButton
+                    title='Back to search'
+                    onPress={() => navigation.goBack()}
+                />
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
 export default Matches;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    safeContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    scrollContainer: {
+        backgroundColor: 'white',
+        height: '100%',
+    },
+});
