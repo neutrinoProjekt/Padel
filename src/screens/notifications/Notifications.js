1bb6b7650a723a829596f12701dca72769f86fef
August <correct-aep9922@gmail.com>
2021-05-06 10:34:32 +0200

    modulised code. css improvments

--- src/screens/notifications/Notifications.js
+++ src/screens/notifications/Notifications.js
@@ -1,3 +1,7 @@
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+/* eslint-disable react/display-name */
+/* eslint-disable react/prop-types */
 import React, {useEffect, useState} from 'react';
 import {StyleSheet, Text, View, Image, TouchableHighlight} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
@@ -7,9 +11,19 @@ import {
     createNotification,
     pressNotification,
     deletNotification,
-    uppdateNotification,
 } from '../../models/Notification';
-import {SafeAreaView} from 'react-native-safe-area-context';
+
+//RAST:
+// 10:00
+// 11:00
+// 12:00
+
+//todo
+//allmän komponent för accept/deny
+//implementera notis för match join
+//implementera notis för tournament join
+
+//refactora
 
 // renders base notification, same for all types
 const NotificationView = (inData) => {
@@ -33,11 +47,10 @@ const NotificationView = (inData) => {
                             <Text style={styles.nHeader}>{item.header}</Text>
                             <View style={{marginTop: 10}}>
                                 <NotificationDetails enabled={extend} item={item}/>
-                                <DeletNotification enabled={extend} item={item}/>
                             </View>
                         </View>
                         <View style={{margin: 10, width: '10%', minWidth: 60}}>
-                            <Text style={styles.nText}>{(fixedDate.toDateString())}</Text>
+                            <Text style={{color: '#bfbfbf'}}>{(fixedDate.toDateString())}</Text>
                         </View>
                     </View>
                     <View style={{width: 8, backgroundColor: item.isnew ? '#00CEB4':'#f7f7f7'}}></View>
@@ -45,24 +58,6 @@ const NotificationView = (inData) => {
             </View>
         </TouchableHighlight>
     );
-};
-
-const DeletNotification = (props) => {
-    if (props.enabled) {
-        return (
-            <View style = {{marginTop: 20}}>
-                <TouchableHighlight onPress={() => {
-                    deletNotification(props.item.id);
-                }}>
-                    <Text style={{width: '100%', textAlign: 'center', padding: 10, backgroundColor: '#F67273', borderRadius: 5, color: '#707070', fontWeight: 'bold'}}>Remove</Text>
-                </TouchableHighlight>
-            </View>
-        );
-    } else {
-        return (
-            <View></View>
-        );
-    }
 };
 
 // this shoud be added to depending on the type
@@ -75,16 +70,25 @@ const NotificationDetails = (props) => {
         switch (item.type) {
         case 'text':
             return (
-                <Text style={styles.nText}>{item.detailText}</Text>
+                <View>
+                    <Text style={styles.nText}>{item.detailText}</Text>
+                    <View style = {{marginTop: 20}}>
+                        <TouchableHighlight onPress={() => {
+                            deletNotification(props.item.id);
+                        }}>
+                            <Text style={{width: '100%', textAlign: 'center', padding: 10, backgroundColor: '#F67273', borderRadius: 5, color: '#707070', fontWeight: 'bold'}}>Remove</Text>
+                        </TouchableHighlight>
+                    </View>
+                </View>
             );
-            // to be added to when the relevant page has been made
         case 'matchJoinRequest':
             return (
-                matchJoinRequest(item)
+                joinRequest({detailText: item.detailText, id: item.id, function: (()=>(console.log('text')))})
             );
-            // to be added to when the relevant page has been made
-        case 'friend request':
-            // to be added to when the relevant page has been made
+        case 'tournamentJoinRequest':
+            return (
+                joinRequest({detailText: item.detailText, id: item.id, function: (()=>(console.log('shoooo')))})
+            );
 
             // more to be added
 
@@ -105,20 +109,22 @@ const NotificationDetails = (props) => {
 };
 
 // måste importera funktionen som låter folk godkänna eller avvisa folk och lägga den istället för console.log
-const matchJoinRequest = (item) => {
+const joinRequest = (props) => {
     return (
         <View>
-            <Text style={styles.nText}>{item.detailText}</Text>
+            <Text style={styles.nText}>{props.detailText}</Text>
             <View style={{flexDirection: 'row', width: '100%', justifyContent: 'space-around', margin: 20}}>
                 <TouchableHighlight onPress={() => {
-                    uppdateNotification({description: 'Denied!', detailText: 'You denied the request'}, item.id);
+                    deletNotification(props.id);
                 }} >
-                    <Text style={{color: '#707070', fontWeight: 'bold', fontSize: '1.5rem'}}>Deny</Text>
+                    <Text style={{color: '#f67273', fontWeight: 'bold'}}>Deny</Text>
                 </TouchableHighlight>
                 <TouchableHighlight onPress={() => {
-                    uppdateNotification({description: 'Accepted!', detailText: 'You accepted the request'}, item.id);
+                    //uppdateNotification({description: 'Accepted!', detailText: 'You accepted the request'}, item.id);
+                    props.function(); // add person to the match
+                    deletNotification(props.id); // remove notification when added
                 }} >
-                    <Text style={{color: '#707070', fontWeight: 'bold', fontSize: '1.5rem'}}>Accept</Text>
+                    <Text style={{color: '#00CEB4', fontWeight: 'bold'}}>Accept</Text>
                 </TouchableHighlight>
             </View>
         </View>
@@ -142,28 +148,28 @@ const Notifications = () => {
     }, []);
 
     return (
-          <View>
-              <TouchableHighlight onPress={() => createNotification({
-                  owner: currentUser.uid,
-                  header: 'Gotta request to join',
-                  description: 'A request to join! now?',
-                  image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Queen_Elizabeth_II_March_2015.jpg/800px-Queen_Elizabeth_II_March_2015.jpg',
-                  detailText: 'urban would like to join your match!',
-                  type: 'matchJoinRequest',
-                  typeDetails: {
-                      joinerId: 'temptemp',
-                  },
-              })}
-              >
-                  <Text>Press to add notification</Text>
-              </TouchableHighlight>
-              <FlatList
-                  data={notificationData}
-                  renderItem={RenderNotification}
-                  keyExtractor={(item) => item.id}
-              />
-              <Text style={styles.nEnd}>No more notifications</Text>
-          </View>
+        <View>
+            <TouchableHighlight onPress={() => createNotification({
+                owner: currentUser.uid,
+                header: 'Bla has requested to join the tournament',
+                description: 'A request to join! now?',
+                detailText: 'lorem ipsum hehj hej max har goda pommes',
+                type: 'tournamentJoinRequest',
+                typeDetails: {
+                    joinerId: 'temptemp',
+                },
+            })
+            }
+            >
+                <Text>Press to add notification</Text>
+            </TouchableHighlight>
+            <FlatList
+                data={notificationData}
+                renderItem={RenderNotification}
+                keyExtractor={(item) => item.id}
+            />
+            <Text style={styles.nEnd}>No more notifications</Text>
+        </View>
     );
 };
 
@@ -191,6 +197,7 @@ const styles = StyleSheet.create({
         width: 60,
         borderRadius: 30,
         margin: 10,
+        backgroundColor: '#00CEB4',
     },
     nEnd: {
         color: '#707070',
